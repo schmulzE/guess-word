3001f305ffb854356fdcf53f0bd33ed799fd5ad7
wisdom eshiet <wisdomeshiet65@gmail.com>
2023-05-24 11:20:48 +0100

    fireworks!!!

---
+++ src/components/Fireworks.tsx
@@ -0,0 +1,52 @@
+import styles from './Fireworks.module.css'
+
+const Fireworks = () => {
+  return (
+    <div>
+    <div className={styles.firework} id={styles.firework1}>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+    </div>
+    <div className={styles.firework} id={styles.firework2}>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+    </div>
+    <div className={styles.firework} id={styles.firework3}>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+        <div className={styles.explosion}></div>
+    </div>
+    </div>
+  )
+}
+
+export default Fireworks
