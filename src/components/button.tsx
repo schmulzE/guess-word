04b247dcd784ec66c060ac93d5be114f7a5654c5
wisdom eshiet <wisdomeshiet65@gmail.com>
2023-05-24 11:10:15 +0100

    generic button component for game

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,15 +1,16 @@
 import React, {Fragment} from 'react'
 
 interface Props{
-  icon: JSX.Element,
+  icon?: JSX.Element,
   text: string,
-  onClick: (event: any) => void
+  onClick: (event: any) => void,
+  className: string
 }
 
 const button: React.FC<Props> = (Props) => {
   return (
     <Fragment>
-    <button className='shadow-2xl w-14 h-14 rounded-full border-2 border-old-whiskey bg-walnut text-center mx-6' onClick={Props.onClick}>
+    <button className={Props.className} onClick={Props.onClick}>
       {Props.icon} 
     <span className='text-xs uppercase tracking-wider'>{Props.text}</span>
     </button>
