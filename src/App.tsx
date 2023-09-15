69fe3d085e96ca2f44f5dde3a0919d9ceac28fbb
wisdom eshiet <wisdomeshiet65@gmail.com>
2023-09-15 01:56:38 +0100

    refactoring: implemented highscore functionality using localstorage & fixed game reset bug

--- src/App.tsx
+++ src/App.tsx
@@ -8,54 +8,31 @@ import { KeyBoards } from "./KeyBoard";
 import NewInput from "./components/NewInput";
 import Modal from "./components/Modal/Modal"
 import "./App.css";
-import Timer from "./components/Timer";
 import Congratulation from "./components/Congratulation/Congratulation";
 import Fireworks from "./components/Fireworks/Fireworks";
 // import useLocalStorage from "../src/useLocalStorage";
+import Pomodoro from "./components/Pomodoro";
 
 function App() {
   // this is the word entered by the user
   const [enteredWord, setEnteredWord] = useState("");
-  const [word, setWord] = useState("qwerty");
+  const [word, setWord] = useState("");
   const [hint, setHint] = useState("");
   const [keyboard, setKeyboard] = useState(KeyBoards);
   // const [score, setScore] = useLocalStorage("score", 0);
   const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
 
   const [popUp, setPopUp] = useState(false);
   const [secondPopUp, setSecondPopUp] = useState(false);
   const [thirdPopUp, setThirdPopUp] = useState(false);
 
-  const [isPaused, setIsPaused] = useState(true);
-  const [secondsLeft, setSecondsLeft] = useState(0);
-
-  const secondsLeftRef = useRef(secondsLeft);
-  const isPausedRef = useRef(isPaused);
+  const [minutes, setMinutes] = useState(5); // Initial work time in minutes
+  const [seconds, setSeconds] = useState(0);
+  const [isBreak, setIsBreak] = useState(false);
+  const [isActive, setIsActive] = useState(false);
 
   const onConfirm = () => setPopUp(false)
-  // useEffect(() => {
-  //   fetchData()
-  // }, [word, hint])
-  
-  // const fetchData = useCallback(() => {
-  //   fetch('https://api.urbandictionary.com/v0/random')
-  //   .then(data => data.json())
-  //   .then(response => {
-  //     const fetchWord  = response.list.word.toLowerCase()
-  //     const fetchHint  = response.list.definition.toLowerCase()
-  //     if (fetchWord.trim().length < 6) {
-  //       // if (/(.).*\1|\d/.test(fetchWord)) {
-  //       if (/(.)\1|\d/.test(fetchWord)) {
-  //         console.log(fetchWord)
-  //         setWord(fetchWord)
-  //         setHint(fetchHint)
-  //       }      
-  //     }else {
-  //       fetchData()
-  //     }
-  //   })
-  // }, [word, hint])
-
 
   async function fetchUrbanDictionaryWords() {
     try {
@@ -78,11 +55,27 @@ function App() {
       console.error("Error fetching words from Urban Dictionary API:", error);
     }
   }
+
+  useEffect(() => {
+    // Load the high score from localStorage when the component mounts
+    const savedHighScore = localStorage.getItem('highScore');
+    if (savedHighScore) {
+      setHighScore(parseInt(savedHighScore));
+    }
+  }, []);
+
+  const increaseScore = () => {
+    const newScore = score + 1;
+    setScore(newScore);
+
+    // Update the high score in localStorage if the current score is higher
+    if (newScore > highScore) {
+      setHighScore(newScore);
+      localStorage.setItem('highScore', newScore.toString());
+    }
+  }
   
   function hasRepeatedCharacters(word: string) {
-    // const regex = /(.)\1+/;
-    // const regex = /(.).*\1/;
-    // return regex.test(word);
     var text = word.split("");
     return text.some(function(v,i,a){
       return a.lastIndexOf(v)!=i;
@@ -100,7 +93,8 @@ function App() {
     let extWord = compareWord;
     if(extWord.trim().length !== 0 && word.trim().length !== 0){  
       if (extWord == word) {
-        setScore(() => score! + 1);
+        // setScore(() => score + 1);
+        increaseScore()
         // this delays clearing the users' input by 1second - just for visual effect.
         setTimeout(() => {
           setEnteredWord("");
@@ -117,13 +111,21 @@ function App() {
     }
   };
 
-  const resetGame = useCallback(() => {
-    setIsPaused(true);
+  const toggleGame = () => {
+    setIsActive(!isActive);
+  };
+
+  const resetGame = () => {
+    setIsActive(false);
+    setIsBreak(false);
+    setMinutes(25);
+    setSeconds(0);
     setWord("");
-    setScore(0); 
-    setSecondsLeft(0);
+    setHint("")
+    setScore(0);
     setSecondPopUp(false);
-  },[isPaused, word, score, thirdPopUp, secondsLeft, setWord])
+  };
+
 
   useEffect(() => {
     if (enteredWord.length !== word.length) return;
@@ -136,7 +138,7 @@ function App() {
         <div className="flex m-3">
           {/* <Fireworks/> */}
           <GiAlarmClock className="w-8 h-8" />
-          <Timer setThirdPopUp={setThirdPopUp} isPaused={isPaused} secondsLeft={secondsLeft} setSecondsLeft={setSecondsLeft} isPausedRef={isPausedRef} secondsLeftRef={secondsLeftRef}/>
+          <Pomodoro minutes={minutes} setMinutes={setMinutes} seconds={seconds} setSeconds={setSeconds} isActive={isActive} setIsActive={setIsActive} isBreak={isBreak} setIsBreak={setIsBreak}/>
         </div>
         <div className="flex m-3">
           <div className="text-2xl uppercase">score</div>
@@ -168,9 +170,9 @@ function App() {
       </div>
       <div className="flex justify-center mt-9">
         <Button className='shadow-2xl w-14 h-14 rounded-full border-2 border-old-whiskey bg-walnut text-center mx-6' icon={<AiOutlineGift className="w-7 h-7 ml-3 pt-1" />} text="hint" onClick={() => setPopUp(true)}></Button>
-         {isPaused
-          ?  <Button className='shadow-2xl w-14 h-14 rounded-full border-2 border-old-whiskey bg-walnut text-center mx-6' icon={<BsPlayFill className="w-11 h-11 ml-1.5" />} text="" onClick={() => { setIsPaused(false); isPausedRef.current = false; fetchUrbanDictionaryWords()} } />
-          : <Button className='shadow-2xl w-14 h-14 rounded-full border-2 border-old-whiskey bg-walnut text-center mx-6' icon={<BsPauseFill className="w-11 h-11 ml-1" />} text="" onClick={() => {  setIsPaused(true); isPausedRef.current = true; setSecondPopUp(true)
+         {!isActive
+          ?  <Button className='shadow-2xl w-14 h-14 rounded-full border-2 border-old-whiskey bg-walnut text-center mx-6' icon={<BsPlayFill className="w-11 h-11 ml-1.5" />} text="" onClick={() => { toggleGame(); fetchUrbanDictionaryWords()} } />
+          : <Button className='shadow-2xl w-14 h-14 rounded-full border-2 border-old-whiskey bg-walnut text-center mx-6' icon={<BsPauseFill className="w-11 h-11 ml-1" />} text="" onClick={() => {  toggleGame(); setSecondPopUp(true)
         } } />}
         <Button
           className='shadow-2xl w-14 h-14 rounded-full border-2 border-old-whiskey bg-walnut text-center mx-6'
@@ -181,20 +183,20 @@ function App() {
       </div>
       <div className="flex justify-center mt-9">
         <NewInput
-          wordCount={word.length}
-          enteredWord={enteredWord}
-          setEnteredWord={setEnteredWord}
+        wordCount={word.length}
+        enteredWord={enteredWord}
+        setEnteredWord={setEnteredWord}
         />
       </div>
       {popUp && <Modal title={'hint'}  onConfirm={onConfirm} children={hint} className2="border-8 bg-old-whiskey border-old-whiskey shadow-[inset_0_2px_4px_rgba(0,0,0,0.6)]" className1="border-walnut border-8 m-4"/>}
       {secondPopUp && <Modal title={'play'} hint={""} onConfirm={() => setSecondPopUp(false)} className2="border-8 bg-old-whiskey border-old-whiskey shadow-[inset_0_2px_4px_rgba(0,0,0,0.6)]" className1="border-walnut border-8 m-4">
-        <Button className='shadow-2xl px-8 rounded-full border-4 my-4 border-ochre bg-walnut text-center mx-6' icon={undefined} text="Resume" onClick={() => { setIsPaused(false); isPausedRef.current = false; setSecondPopUp(false)} } />
+        <Button className='shadow-2xl px-8 rounded-full border-4 my-4 border-ochre bg-walnut text-center mx-6' icon={undefined} text="Resume" onClick={() => { toggleGame(); setSecondPopUp(false)} } />
         <Button className='shadow-2xl px-8 rounded-full border-4 border-ochre bg-walnut text-center mx-6' icon={undefined} text="Quit" onClick={resetGame} />
       </Modal>}
        {thirdPopUp && <Modal title="" onConfirm={() => {setThirdPopUp(false);} } className1="" className2="mt-8">
         <Congratulation title={"High Score"} score={score}/>
         <Fireworks/>
-        <Button className='shadow-2xl pb-1 px-8 rounded-full border-4 my-8 border-ochre bg-walnut text-center mx-6 ' icon={undefined} text="Play Again" onClick={() => {setIsPaused(true); setSecondsLeft(0); setScore(0); setWord(""); setThirdPopUp(false)} } />
+        <Button className='shadow-2xl pb-1 px-8 rounded-full border-4 my-8 border-ochre bg-walnut text-center mx-6 ' icon={undefined} text="Play Again" onClick={() => { resetGame(); setThirdPopUp(false)} } />
        </Modal>}
     </div>
   );
