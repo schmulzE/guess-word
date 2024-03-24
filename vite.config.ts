ff99018b6ce548f70974a86001b826330d8a60e4
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-03-24 14:10:56 +0100

    implemented and merged vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig as defineViteConfig, mergeConfig } from 'vite';
+import { defineConfig as defineVitestConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+const viteConfig = defineViteConfig({
+  plugins: [react()],
+});
+
+const vitestConfig = defineVitestConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./tests/setup.ts'],
+
+  },
+});
+
+export default mergeConfig(viteConfig, vitestConfig);
