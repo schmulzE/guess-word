616a0ce45decd2abf302d2c435e2b4f975837b71
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-10-10 13:49:16 +0100

    cleaned up code, facilitating readability and maintainability

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,32 +1,21 @@
-import { ReactNode } from 'react'
-import ReactDom from 'react-dom'
+import { ReactNode } from 'react';
+import ReactDom from 'react-dom';
+import './modal.css';
 
-const MODAL_STYLES = {
-  position: 'fixed' as 'fixed',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  backgroundColor: '#FFF',
-  zIndex: 1000
+interface ModalProps {
+  open: boolean;
+  children: ReactNode;
+  modalClass: string;
+  data_testid?: string
 }
 
-const OVERLAY_STYLES = {
-  position: 'fixed' as 'fixed',
-  top: 0,
-  left: 0,
-  right: 0,
-  bottom: 0,
-  backgroundColor: 'rgba(0, 0, 0, .7)',
-  zIndex: 1000
-}
-
-export default function Modal({ open, children }: {open: boolean, children: ReactNode}) {
+export default function Modal({ open, children, modalClass, data_testid }: ModalProps) {
   if (!open) return null
 
   return ReactDom.createPortal(
     <>
-      <div style={OVERLAY_STYLES}  className='overflow-y-hidden'/>
-      <div style={MODAL_STYLES} className='w-full md:w-3/4 h-fit'>
+      <div className='overflow-y-hidden backdrop'/>
+      <div data-testid={data_testid} className={modalClass + ' modal'}>
         {children}
       </div>
     </>,
